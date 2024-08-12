6df8c77b0e94d64dd1ce1b0f30f2880351227a4b
Neeraj Yadav <neeraj95578@gmail.com>
2024-08-12 20:23:26 +0530

    product ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProducts } from "@/lib/actions";
 import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProducts();
+
   return (
     <>
       <section className=" px-6 md:px-20 py-24">
@@ -28,7 +32,7 @@ const Home = () => {
 
             <Searchbar />
           </div>
-          
+
           <HeroCarousel />
         </div>
       </section>
@@ -36,11 +40,18 @@ const Home = () => {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
-        <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone 15", "book", "Sneakers"].map((product) => (
-            <div>{product}</div>
+              <div>
+                {allProducts?.map((product) =>(
+                  <ProductCard key={product._id} product={product}/>
+                ))}
+              </div>
+
+        {/* <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            // <ProductCard key={product._id} product={product} />
+            <ProductCard key={product._id} product={product}/>
           ))}
-        </div>
+        </div> */}
       </section>
     </>
   );
--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,37 +1,40 @@
-"use server";
+"use server"
+
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+// import { User } from "@/types";
+// import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if (!productUrl) return;
+  if(!productUrl) return;
 
   try {
     connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
-    if (!scrapedProduct) return;
+    if(!scrapedProduct) return;
 
     let product = scrapedProduct;
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-    if (existingProduct) {
-      const updatePriceHistory = [
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
         ...existingProduct.priceHistory,
-        { price: scrapedProduct.currentPrice },
-      ];
+        { price: scrapedProduct.currentPrice }
+      ]
 
       product = {
         ...scrapedProduct,
-        // priceHistory: updatePriceHistory,
-        lowestPrice: getLowestPrice(updatePriceHistory),
-        highestPrice: getHighestPrice(updatePriceHistory),
-        // averagePrice: getAveragePrice(updatePriceHistory),
-      };
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        // averagePrice: getAveragePrice(updatedPriceHistory),
+      }
     }
 
     const newProduct = await Product.findOneAndUpdate(
@@ -42,7 +45,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
-    throw new Error(`Failed to create/update product : ${error.message}`);
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
@@ -52,9 +55,21 @@ export async function getProductById(productId: string) {
 
     const product = await Product.findOne({ _id: productId });
 
-    if (!product) return null;
+    if(!product) return null;
 
     return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllProducts() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
   } catch (error) {
     console.log(error);
   }
