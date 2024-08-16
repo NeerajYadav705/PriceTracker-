c2e8ca6c25dc6158ff39b5390026e476102c701d
Neeraj Yadav <neeraj95578@gmail.com>
2024-08-16 20:26:24 +0530

    changes in searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const Home = async () => {
       <section className="px-6 md:px-20 py-24">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center"> 
-            {/* <p className="small-text">
+            <p className="small-text">
               Smart Shopping Starts Here:
               <Image 
                 src="/assets/icons/arrow-right.svg"
@@ -20,11 +20,11 @@ const Home = async () => {
                 width={16}
                 height={16}
               />
-            </p> */}
+            </p>
 
             <h1 className="head-text">
               Unleash the Power of
-              <span className="text-lime-500"> PriceWise</span>
+              <span className="text-primary"> PriceWise</span>
             </h1>
 
             <p className="mt-6">
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,28 +1,29 @@
-"use client";
+"use client"
 
-import { scrapeAndStoreProduct } from "@/lib/actions";
-import React, { FormEvent, useState } from "react";
+import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from 'react'
 
 const isValidAmazonProductURL = (url: string) => {
   try {
     const parsedURL = new URL(url);
     const hostname = parsedURL.hostname;
-    if (
-      hostname.includes("amazon.com") ||
-      hostname.includes("amazon.") ||
-      hostname.endsWith("amazon")
+
+    if(
+      hostname.includes('amazon.com') || 
+      hostname.includes ('amazon.') || 
+      hostname.endsWith('amazon')
     ) {
       return true;
     }
   } catch (error) {
     return false;
   }
+
   return false;
-};
+}
 
 const Searchbar = () => {
-  const [searchPrompt, setSearchPrompt] = useState("");
-
+  const [searchPrompt, setSearchPrompt] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
@@ -30,39 +31,42 @@ const Searchbar = () => {
 
     const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if (!isValidLink) return alert("Please provide a valid Amazon link");
+    if(!isValidLink) return alert('Please provide a valid Amazon link')
 
     try {
       setIsLoading(true);
 
-      //scaraping the product page
+      // Scrape the product page
       const product = await scrapeAndStoreProduct(searchPrompt);
     } catch (error) {
-      console.error("An error occurred while scraping the product:", error);
+      console.log(error);
     } finally {
       setIsLoading(false);
     }
-  };
+  }
 
   return (
-    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
-      <input
+    <form 
+      className="flex flex-wrap gap-4 mt-12" 
+      onSubmit={handleSubmit}
+    >
+      <input 
         type="text"
         value={searchPrompt}
         onChange={(e) => setSearchPrompt(e.target.value)}
-        placeholder="Enter the product link"
+        placeholder="Enter product link"
         className="searchbar-input"
       />
 
-      <button
-        type="submit"
+      <button 
+        type="submit" 
         className="searchbar-btn"
-        disabled={searchPrompt === ""}
+        disabled={searchPrompt === ''}
       >
-        {isLoading ? "Searching..." : "Search"}
+        {isLoading ? 'Searching...' : 'Search'}
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default Searchbar;
+export default Searchbar
